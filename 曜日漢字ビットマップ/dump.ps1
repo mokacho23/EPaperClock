20e52945601500efc0f07dc5001f810eb635eea4
olive <olivemoon23@gmail.com>
2024-07-08 22:26:15 +0900

    曜日の漢字を微調整

---
+++ 曜日漢字ビットマップ/dump.ps1
@@ -0,0 +1,11 @@
+$x = Get-Content -AsByteStream "C:\Data\IT\git\EPaperClock\曜日漢字ビットマップ\曜日漢字ビットマップ４.bmp"
+
+for ($i = 0x3E; $i -lt $x.Length; $i++) {
+
+    if ((($i - 0x3E) % 4) -eq 3) {
+        Write-Host ""
+    }
+    else {
+        Write-Host -NoNewline ('0x{0:x2}, ' -f ((-bnot $x[$i]) -band 0x000000FF))
+    }
+}
