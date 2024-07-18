27e47faf698e57f76eb5112fb5fcb1198c36dd33
olive <olivemoon23@gmail.com>
2024-07-18 21:02:34 +0900

    曜日の文字を大きく

--- 曜日漢字ビットマップ/dump.ps1
+++ 曜日漢字ビットマップ/dump.ps1
@@ -2,10 +2,8 @@ $x = Get-Content -AsByteStream "C:\Data\IT\git\EPaperClock\曜日漢字ビット
 
 for ($i = 0x3E; $i -lt $x.Length; $i++) {
 
+    Write-Host -NoNewline ('0x{0:x2}, ' -f ((-bnot $x[$i]) -band 0x000000FF))
     if ((($i - 0x3E) % 4) -eq 3) {
         Write-Host ""
-    }
-    else {
-        Write-Host -NoNewline ('0x{0:x2}, ' -f ((-bnot $x[$i]) -band 0x000000FF))
     }
 }
